2f910fbe6b4173d524f2db5a3a3d76f1a477912a
Марина Беркута <yryry_21@Marinas-MacBook-Air.local>
2025-07-27 18:27:31 +0300

    Добавила requirements.txt, поправила замечания

--- duckduckgo.py
+++ duckduckgo.py
@@ -1,9 +1,9 @@
 from selene import browser, be, have
 
-def test_duckduckgo(opening_browser):
+def test_duckduckgo(open_duckduckgo):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('html').should(have.text('GitHub - yashaka/selene: User-oriented Web UI browser tests in Python'))
 
-def test_search(opening_browser):
+def test_search(open_duckduckgo):
     browser.element('[name="q"]').type('hkjhkhkjhkjhkjgjhgfjgfghfhgfhfgfghfgh').press_enter()
     browser.element('html').should(have.text('По запросу «hkjhkhkjhkjhkjgjhgfjgfghfhgfhfgfghfgh» ничего не найдено.'))
